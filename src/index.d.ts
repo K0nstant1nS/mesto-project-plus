3817c8145ef155b14a845a43f5f69001eb4b2681
Константин Cоловьев <K1kollov@gmail.com>
2023-07-10 16:46:10 +0300

    last fixes

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,11 @@
+interface IUser {
+  _id: string
+}
+
 declare global {
   namespace Express { // eslint-disable-line no-unused-vars
     export interface Request {
-      user: {
-        _id: string
-      }
+      user: IUser
     }
   }
 }
